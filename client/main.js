6de32fc782e996577f0b2904817d7f79fcc77e85
Joe Mitchell <joemit99@gmail.com>
2019-02-10 19:23:32 +0000

    Created basic GUI layout for the application, working with chat and groups

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 800})
+  win = new BrowserWindow({width: 1200, height: 800, minHeight : 400, minWidth : 900, frame : false})
   //query existing cookies
   session.defaultSession.cookies.get({}, (error, cookies) => {
     console.log(error, cookies)
